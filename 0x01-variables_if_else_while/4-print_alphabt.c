2a62367f6a1c789dac5f02e558200c27b676e15c
tarik khadri <tarikkhadri11@gmail.com>
2023-09-08 20:21:46 +0100

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description:positive or negative
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alpha ;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
+		if (alpha != 'e' && ch != 'q')
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+	alpha('\n');
+	return (0);
+}
