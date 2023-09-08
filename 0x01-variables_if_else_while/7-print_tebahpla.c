0c010d06662d94800a533e841e2fe82ee779d47b
tarik khadri <tarikkhadri11@gmail.com>
2023-09-08 22:04:38 +0100

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description:positive or negative
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char x;
+for (x = 'z'; x < 'a'; x--)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
