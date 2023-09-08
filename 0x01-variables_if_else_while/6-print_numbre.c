71297b82edf764786d0fbcd0e0d32094b04e0d0d
tarik khadri <tarikkhadri11@gmail.com>
2023-09-08 20:58:45 +0100

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numbre.c
@@ -0,0 +1,19 @@
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description:positive or negative
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+for (x = '0'; x <= 10 ; x++)
+{
+	putchar(x);
+}
+putchar('\n');
+return (0);
+}
