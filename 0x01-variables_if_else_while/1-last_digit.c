ede845fe7d625df73bda15fe39b8a8ae5fb672ca
tarik khadri <tarikkhadri11@gmail.com>
2023-09-08 18:19:46 +0100

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description:positive or negative
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n % 10 > 5)
+	{
+		printf("%d and is greater than 5\n", n);
+	}
+	else if (n % 10 == 0)
+	{
+		printf("%d and is 0\n", n);
+	}
+	else if (n % 10)
+	{
+		printf("%d and is less than 6 and not 0\n", n);
+	}
+	return (0);
+}
