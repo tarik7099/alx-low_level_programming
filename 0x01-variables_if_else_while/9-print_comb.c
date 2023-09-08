cfcae7fbc00c980fb4dbc00c2d89c3c22d1719ae
tarik khadri <tarikkhadri11@gmail.com>
2023-09-08 22:32:49 +0100

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 int x;
-for (x = 0 ; x < 10 ; x++)
+for (x = 0 ; x <= 9 ; x++)
 {
 putchar(x);
 if (x != 9)
-	putchar(',');
 	putchar('');
+	putchar(',');
 }
 putchar('\n');
 return (0);
