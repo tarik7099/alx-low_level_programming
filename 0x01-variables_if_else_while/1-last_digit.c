ca9b93147f36d0ca5ba78188a07ef75d1713353b
tarik khadri <tarikkhadri11@gmail.com>
2023-09-08 18:33:40 +0100

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n % 10 > 5)
 	{
-		printf("%d and is greater than 5\n", n);
+		printf("and is greater than 5\n");
 	}
 	else if (n % 10 == 0)
 	{
-		printf("%d and is 0\n", n);
+		printf("and is greater than 5\n");
 	}
 	else if (n % 10 < 6 && n % 10 != 0)
 	{
-		printf("%d and is less than 6 and not 0\n", n);
+		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
 }
