6116b469877db8aa837dee0a9dfdea068ce789f5
tarik khadri <tarikkhadri11@gmail.com>
2023-09-11 12:37:55 +0100

    /0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,25 @@
 * Description:positive or negative
 *
 * Return: Always 0 (Success)
-*/	
+*/
 int main(void)
 {
 	int a, b;
 
-	for (a = 0; a < 9; a++)
+for (a = 0; a < 10; a++)
+{
+	for (b = a + 1; a < 10; b++)
 	{
-		for (b = a + 1; a < 10; b++)
-		{
-			putchar(a + '0');
-			putchar(b + '0');
+		putchar(a + '0');
+		putchar(b + '0');
 
-			if (a != 8 || b != 9)
-			{
-			putchar(',');
-			putchar(' ');
-			}
+		if (a != 8 || b != 9)
+		{
+		putchar(',');
+		putchar(' ');
 		}
 	}
+}
 
 	putchar('\n');
 return (0);
