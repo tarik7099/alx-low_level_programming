5f94cb45da29d33b7085e4636e65ca5064e13c0b
tarik khadri <tarikkhadri11@gmail.com>
2023-09-12 22:24:57 +0100

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
-int main ()
-{
-void putcahr_print();
-putchar_print();
-}
-void putchar_print()
-{
-printf("_putchar")
+#include "main.h"
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
