4b74cae9f047c069ee0e63ac66cfb2404c953e18
tarik khadri <tarikkhadri11@gmail.com>
2023-09-13 01:51:40 +0100

    /0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alpha - prints the alphabet in lowercase, followed by a new line.
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-void print_alpha(void)
+void print_alphabet(void)
 {
 	char let1;
 
