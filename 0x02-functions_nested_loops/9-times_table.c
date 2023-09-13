3b4916546af2cb739621bf7fb6d1f23c14865dd5
tarik khadri <tarikkhadri11@gmail.com>
2023-09-13 02:29:05 +0100

    0x02-functions_nested_loops0x02-functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,17 +12,16 @@ for (call = 0; call <= 9; call++)
 	_putchar('0');
 
 for (mult = 1; mult <= 9; mult++)
-{
-	_putchar(',');
-	_putchar(' ');
+{_putchar(',');
+_putchar(' ');
 total = call * mult;
 if (total <= 9)
 	_putchar(' ');
 else
-	_putchar((total / 10) + '0');
+_putchar((total / 10) + '0');
 
-	_putchar((total % 10) + '0');
+_putchar((total % 10) + '0');
 }
-	_putchar('\n');
+_putchar('\n');
 }
 }
