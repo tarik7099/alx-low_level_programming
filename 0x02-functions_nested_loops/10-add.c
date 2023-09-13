0b391aab80e040080efccc19f0b262c339e9acd9
tarik khadri <tarikkhadri11@gmail.com>
2023-09-13 02:31:14 +0100

    0x02-functions_nested_loops0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - fonctionAdds two integers.
+ * @n1: The first integer 
+ * @n2: The second integer 
+ *
+ * Return: The result of the addition.
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
