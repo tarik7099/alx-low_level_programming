93f767bee279089619170e8fe7490bb82b14b186
tarik khadri <tarikkhadri11@gmail.com>
2023-09-13 01:50:00 +0100

    /0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet in lowercase,
+ * print_alpha - prints the alphabet in lowercase, followed by a new line.
  */
-void  print_alphabet_x10(void)
+void print_alpha(void)
 {
 	char let1;
 
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - 10 times the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int counter = 0;
+	char alpha;
+
+	while (counter++ <= 9)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
+	}
+}
