5dbba25ab1b27d3b9251c4d8ea4e3650298250cd
tarik khadri <tarikkhadri11@gmail.com>
2023-09-13 02:09:27 +0100

    0x02-functions_nested_loops0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*_islower - unction controls if a character is in lowercase
+*Return: return 0 or 1
+*/
+int _islower(int x)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
