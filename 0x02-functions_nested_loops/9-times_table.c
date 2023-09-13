868ad5827df751afb791e5dd131155aa4f49c85f
tarik khadri <tarikkhadri11@gmail.com>
2023-09-13 02:27:28 +0100

    0x02-functions_nested_loops0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*times_table -  function prints the 9 times table
+*
+*Return: return  1
+*/
+void times_table(void)
+{
+int call, mult, total;
+for (call = 0; call <= 9; call++)
+{
+	_putchar('0');
+
+for (mult = 1; mult <= 9; mult++)
+{
+	_putchar(',');
+	_putchar(' ');
+total = call * mult;
+if (result <= 9)
+	_putchar(' ');
+else
+	_putchar((total / 10) + '0');
+
+	_putchar((total % 10) + '0');
+}
+	_putchar('\n');
+}
+}
