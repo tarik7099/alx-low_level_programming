9eedb546d67a1b6a0faa048dbe2c0fe43cf95871
tarik khadri <tarikkhadri11@gmail.com>
2023-09-13 02:02:18 +0100

    0x02-functions_nested_loops0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,9 @@
 #include "main.h"
-
+#include "main.h"
 /**
- * print_alphabet_x10 - 10 times the alphabet
+ * main - check the code.
+ * print_alphabet_x10 -  function that prints 10 times the alphabet
+ * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
