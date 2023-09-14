562c17d78298d362e28bca6cf8d9814b5c15e68e
tarik khadri <tarikkhadri11@gmail.com>
2023-09-14 23:03:08 +0100

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/**
+ * print_numbers - function that prints the numb * ers, from 0 to 9
+ * main - check the code
+ * Return: Always 0.
+ */
+void print_numbers(void);
+{
+int x;
+while (x >= 0 && x <= 9)
+	}
+	num++;
+	_putchar(x + 48)
+	}
+putchar('\n');
+}
