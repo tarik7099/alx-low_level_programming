078268d1879b6d78087c922090d5aff1a31fc360
tarik khadri <tarikkhadri11@gmail.com>
2023-09-15 00:18:59 +0100

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,14 +12,13 @@ void more_numbers(void)
 	{
 		for (num1 = 0; num1 <= 14; num1++)
 		{
-			count = num1 ;
-
-			if (num1 >9)
+			num = count;
+			if (num1 > 9)
 			{
 				_putchar(1 + 48);
 				num1 = count % 10;
 			}
-			_putchar(48 + count);
+			_putchar(48 + num1);
 		}
 		_putchar('\n');
 	}
