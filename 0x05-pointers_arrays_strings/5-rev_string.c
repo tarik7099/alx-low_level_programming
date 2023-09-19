bf7458b9ca92af54f9be5ac79955e4e44bb9d7ae
tarik khadri <tarikkhadri11@gmail.com>
2023-09-19 02:00:20 +0100

    /0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string
+ * rev_string - a function that reverses a string.
  * @s: string to be reversed
  *
  * Return : Nothing
@@ -12,12 +12,10 @@ void rev_string(char *s)
 	int i, length;
 
 	for (length = 0 ; s[length] != '\0'; length++)
-		;
-
 	for (i = 0 ; i < length / 2 ; i++)
 	{
 		temp = s[i];
 		s[i] = s[length - 1 - i];
-		s[length - 1 -i] = temp;
+		s[length - 1 - i] = temp;
 	}
 }
