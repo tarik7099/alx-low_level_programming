af3e41436f26bae4d6abe957d51bba4863ae0b3e
tarik khadri <tarikkhadri11@gmail.com>
2023-09-21 23:09:23 +0100

    /0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strncat - concatenate strings, defining the size of second string.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int lenD;
+
+	for (lenD = 0; lenD < n && src[lenD] != '\0'; lenD++)
+		dest[lenD] = src[lenD];
+
+	while (lenD < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
