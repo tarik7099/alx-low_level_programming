56f9f022b602bfe5023f18803c5f42ca69b24e23
tarik khadri <tarikkhadri11@gmail.com>
2023-09-25 16:38:40 +0100

    /0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _memset - fills memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ */
+ har *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int memS;
+
+	for (memS = 0; memS < n; memS++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
