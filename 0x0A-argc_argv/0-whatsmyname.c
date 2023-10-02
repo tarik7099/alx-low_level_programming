9a0e93faf09fd8efaf7ca61757b49e3cb45da870
tarik khadri <tarikkhadri11@gmail.com>
2023-10-02 14:18:18 +0100

    /0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  * @argv: char list
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char const *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
