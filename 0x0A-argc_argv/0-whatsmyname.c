cb6c8aff7bf7ec11a2a3513478a23ec7e0d6dabf
tarik khadri <tarikkhadri11@gmail.com>
2023-10-02 14:14:13 +0100

    /0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Write a program that multiplies two numbers.
+ * @argc: int
+ * @argv: char list
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char const *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
