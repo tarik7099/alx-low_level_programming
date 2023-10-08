713fe888e5be125606e7757a3701b28abcd3f4ec
tarik khadri <tarikkhadri11@gmail.com>
2023-10-08 13:16:50 +0100

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,54 +1,39 @@
 #include <stdlib.h>
-#include "main.h"
+#include <string.h>
 
 /**
- * string_nconcat - Write a function that concatenates two strings.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The length of copy in s2
- *
- * Return: A pointer to the newly allocated concatenated string.
- *         If malloc fails or n is greater than or equal to the length of s2,
- *         return NULL.
+ * string_nconcat - function Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ * @n: length of s2
+ * Return: Pointer to newly allocated space in memory containing concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int len1 = 0, len2 = 0, i, j;
-char *result;
+    unsigned int len1, len2, i;
+    char *result;
 
-if (s1 == NULL)
-{
-	s2 = "";
-}
-if (s2 == NULL)
-	s2 = "";
-while (s1[len1] != '\0')
-{
-	len1++;
-}
-while (s2[len2] != '\0')
-{
-	len2++;
-}
-if (n >= len2)
-	n = len2;
-result = malloc(sizeof(char) * (len1 + n + 1));
-if (result == NULL)
-{
-return (NULL);
-}
+    if (s1 == NULL)
+        s1 = "";
 
-for (i = 0; i < len1; i++)
-{
-result[i] = s1[i];
-}
-for (j = 0; j < n; j++, i++)
-{
-result[i] = s2[j];
-}
+    if (s2 == NULL)
+        s2 = "";
 
-result[i] = '\0';
+    while(s1[len1] != '\0')
+	    len1++;
+    while(s2[len2] != '\0')
+            len2++;
+    if (n >= len2)
+        n = len2;
+ result = malloc((len1 + n + 1) * sizeof(char));
+    if (result == NULL)
+        return NULL;
+    for (i = 0; i < len1; i++)
+        result[i] = s1[i];
 
-return (result);
+    for (i = 0; i < n; i++)
+        result[len1 + i] = s2[i];
+result[len1 + n] = '\0';
+
+    return result;
 }
-
