64d50d0e58d4f697325a6b7940aedfa5820f7bbc
tarik khdri <tarikkhadri11@gmail.com>
2023-10-10 22:05:29 +0100

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a dog struct
+ * @name: dogs name
+ * @age:dogs age
+ * @owner: owne
+ * Description: struct for the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 #endif /* DOG_H */
