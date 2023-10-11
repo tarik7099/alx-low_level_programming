96dc7487e1330c0a75b46de00b68dd3648fec748
tarik khdri <tarikkhadri11@gmail.com>
2023-10-11 01:35:27 +0100

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,18 +10,53 @@
  * @owner: owner dogs.
  * Return: A pointer to the new dog, or NULL on failure.
  */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *dog_ptr;
+int len1, len2;
 if (owner == NULL || name == NULL)
 	return (NULL);
 dog_ptr = malloc(sizeof(dog_t));
 if (dog_ptr == NULL)
 	return (NULL);
-dog_ptr->name = strdup(name);
-dog_ptr->owner = strdup(owner);
+len1 = _strlen(name);
+len2 = _strlen(owner);
 
+dog_ptr->name = malloc(len1 + 1);
+dog_ptr->owner = malloc(len2 + 1);
 if (dog_ptr->name == NULL || dog_ptr->owner == NULL)
 {
 free(dog_ptr->name);
