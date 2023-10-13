c4d384906093cf3b7024259e07784f8e78c02c2a
tarik khdri <tarikkhadri11@gmail.com>
2023-10-13 03:15:12 +0100

    0x10-variadic_functions$

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all - Prints anything.
+ * @format: A list of types of arguments passed to the function.
+ * ...: The variable-length list of parameters.
+ *
+ * Allowed types:
+ * c: char
+ * i: integer
+ * f: float
+ * s: char * (if the string is NULL, print (nil) instead)
+ *
+ * Any other char will be ignored.
+ * Prints a new line at the end of the function.
+ */
+
+void print_all(const char * const format, ...)
+{
+va_list args;
+char *sepa = "";
+unsigned int i = 0;
+char *str;
+va_start(args, format);
+while (format && format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", sepa, va_arg(args, int));
+break;
+case 'i':
+printf("%s%d", sepa, va_arg(args, int));
+break;
+case 'f':
+printf("%s%f", sepa, va_arg(args, double));
+break;
+case 's':
+{
+str = va_arg(args, char *);
+if (str == NULL)
+{
+printf("%s(nil)", sepa);
+}
+else
+{
+printf("%s%s", sepa, str);
+
+}
+}
+break;
+default:
+break;
+}
+sepa = ", ";
+i++;
+}
+printf("\n");
+va_end(args);
+}
+
