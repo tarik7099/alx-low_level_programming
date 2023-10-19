88c9c4c210b07523c65edd71dd0113c89fb31fbb
tarik <tarikkhadri11@gmail.com>
2023-10-20 00:47:55 +0100

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,13 @@ size_t print_list(const list_t *h)
 size_t Prlist = 0;
 while (h != NULL)
 	{
-	if (h->str != NULL)
+	if (h->str == NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+	printf("[0] (nil)\n");
 	}
 	else
 	{
-	 printf("[0] (nil)\n");
+	printf("[%d] %s\n", h->len, h->str);
 	}
 
 	h = h->next;
