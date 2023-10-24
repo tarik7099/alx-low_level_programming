69211a98fe67b73854754fa7e3f8568bf01712ad
tarik <tarikkhadri11@gmail.com>
2023-10-24 23:42:13 +0100

    linked list all fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,3 +1,4 @@
+#include "lists.h"
 /**
  * get_nodeint_at_index - Returns the nth node of a listint_t linked list.
  * @head: A pointer to the head
@@ -6,7 +7,7 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-unsigned int i = 0;
+unsigned int count = 0;
 while (head != NULL)
 {
 if (count == index)
