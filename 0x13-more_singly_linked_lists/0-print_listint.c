5125072035f45bb86fb6db627d1b2e9ad34e1642
tarik <tarikkhadri11@gmail.com>
2023-10-24 01:07:21 +0100

    linkedlistmore

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,12 +7,12 @@
 */
 size_t print_listint(const listint_t *h)
 {
-int node_count = 0;
+int node_c = 0;
 while (h != NULL)
 {
 	printf("%d\n", h->n);
 	h = h->next;
-	node_count++;
+	node_c++;
 }
-return node_count;
+return (node_c);
 }
