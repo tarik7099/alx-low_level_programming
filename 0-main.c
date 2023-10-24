2312c44b9d96a977605983b9b13ebdef1712d56f
tarik <tarikkhadri11@gmail.com>
2023-10-24 11:06:09 +0100

    linked list

--- 0-main.c
+++ 0-main.c
@@ -5,28 +5,36 @@
 
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-    listint_t *head;
-    listint_t *new;
-    listint_t hello = {8, NULL};
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
     size_t n;
 
     head = &hello;
-    new = malloc(sizeof(listint_t));
+    new = malloc(sizeof(list_t));
     if (new == NULL)
     {
         printf("Error\n");
         return (1);
     }
-    new->n = 9;
+    new->str = strdup("Hello");
+    new->len = 5;
     new->next = head;
     head = new;
-    n = print_listint(head);
+    n = print_list(head);
     printf("-> %lu elements\n", n);
+
+    printf("\n");
+    free(new->str);
+    new->str = NULL;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+
     free(new);
     return (0);
 }
