bb9ff5a5985d1619e881e394e0832b7ae47c5a6f
tarik <tarikkhadri11@gmail.com>
2023-10-26 20:58:33 +0100

    bitwise

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,11 +7,12 @@
  * Return: 1 if the operation was successful, or -1 if an error occurred.
  */
 
-int clear_bit(unsigned long int *n, unsigned int index) {
+int clear_bit(unsigned long int *n, unsigned int index)
+{
 if (index >= sizeof(unsigned long int) * 8 || n == NULL)
 {
-return -1;
+return (-1);
 }
-    *n &= ~(1UL << index);
-return 1;
+*n &= ~(1UL << index);
+return (1);
 }
