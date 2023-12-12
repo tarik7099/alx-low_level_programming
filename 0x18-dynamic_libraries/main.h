7a5baed44e7feed8f26e4783b3a3eb5bdc2661d5
tarik khadri <tarikkhadri11@gmail.com>
2023-12-12 11:55:48 -0800

    fix

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,4 +1,3 @@
-:
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -26,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif
