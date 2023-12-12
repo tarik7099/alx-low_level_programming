e6e9484ebb6f72c2baed067a4b8bb09869be0a4d
tarik khadri <tarikkhadri11@gmail.com>
2023-12-12 11:58:22 -0800

    fix

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pednatic  -c -fPIC *.c
-gcc -shared  -o liball.so *.o
-gcc *.c  . -all
-
-export LD_LIBRARY_PATH=.:LD_LIBRARY_PATH
+gcc -f -fPic  *.c
+gcc -shared *.o -o liball.so
