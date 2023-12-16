18a3f87986da35a077384897a97155b041b21219
root <tarikkhadri11@gmail.com>
2023-12-15 16:51:58 -0800

    n9

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -3,8 +3,8 @@
 #include <unistd.h>
 int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 - 9\n", 17);
-	write(1, "congratulations, you win Jackpot!\n", 35);
-	exit(EXIT_SUCCESS);
+write(1, "9 8 10 24 75 - 9\n", 17);
+write(1,"Congratulations, you win the Jackpot!\n", 38);
+exit(EXIT_SUCCESS);
 }
 
