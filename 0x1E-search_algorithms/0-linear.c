4e747f08821b70fdaf749d622d83f3861b7e74de
TARIK KHADRI <tarikkhadri112gmail.com>
2024-04-10 22:01:02 +0000

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,15 +15,15 @@ size_t i;
 
 if (array == NULL)
 {
-return -1;
+return (-1);
 }
 for (i = 0; i < size; i++)
 {
 printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 if (array[i] == value)
 {
-return i;
+return (i);
 }
 }
-    return -1;
+return (-1);
 }
