bcd8341899325cd1375d4d7bea5f1ed2b85af614
TARIK KHADRI <tarikkhadri112gmail.com>
2024-04-12 00:10:05 +0000

    f

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
-#!/bin/bas
-h
+#!/bin/bash
+wget -P /tmp https://github.com/tarik7099/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libnewdynamic.so
+export LD_PRELOAD=/tmp/libnewdynamic.so
